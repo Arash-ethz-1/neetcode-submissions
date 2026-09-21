3e383c9ea695bf4871c2f83155ee6f3ed1dc1ec0
Arash Bayat <abayat@ethz.ch>
2026-09-21 14:29:32 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProf = 0;
+        int n = prices.length;
+
+        for (int i = 0; i < n; i++){
+            for( int j = i+1 ; j < n; j++){
+                if((prices[j] - prices[i]) > maxProf){
+                    maxProf = prices[j] - prices[i];
+                }
+            }
+        }
+
+        return maxProf;
+    }
+}
